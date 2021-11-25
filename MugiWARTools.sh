b6df169073450685898db60a2aff129407ac8aff
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-11-25 09:43:55 +0700

    Update and rename MugiwarTools.sh to MugiWARTools.sh

---
+++ MugiWARTools.sh
@@ -0,0 +1,218 @@
+# eittss mau recode?
+# hargain authornya
+# subscribe T1T4N TZY
+
+green='\033[0;32m'
+greenlight='\033[32;1m'
+red='\033[31;1m'
+whhite='\033[31;1m'
+black='\033[30;1m'
+yellow='\033[33;1m'
+
+#regis dulu
+clear
+echo $green "###############"
+echo $green "##           ##"
+echo $green "###############"
+echo $red "Utamakan Subscribe, T1T4N TZY"
+echo $red "Passwordnya Dan Usernamenya Ada Di Atas"
+    echo $yellow "Usernamenya Apa??"
+    echo $yellow "Passwordnya Apa??"
+     read -p "User =>" user
+     read -p "Pass =>" pass
+        sleep 0.1
+#usernamenya sob
+if [ $user = "admin" ]
+   then
+      echo $red "#####################"
+   echo $red "##"$yellow "Username Succes"$red "##"
+   echo $red "#####################"
+   sleep 0.1
+   else
+   echo $black "#####################"
+   echo $black "##"$red "Username Failedd"$black "##"
+   echo $black "####################"
+   sleep 3
+   sh Jeder.sh
+   sleep 0.1
+fi
+#passwordnya sobb
+if [ $pass = "PokeDiger1" ]
+   then
+   echo $red "#####################"
+   echo $red "##"$yellow "Password Succes"$red "##"
+   echo $red "#####################"
+   sleep 0.1
+   else
+   echo $red "######################"
+   echo $red "##"$black "Yah Salah Dehhhh"$red "##"
+   echo $red "######################"
+   sleep 3
+      sh Jeder.sh
+   sleep 0.1
+fi
+clear
+# tampilannya
+echo $yellow "######################"
+echo $yellow "####"$black "000"$yellow "####"$black "000"$yellow "####"
+echo $yellow "####"$black "000"$yellow "####"$black "000"$yellow "####"
+echo $yellow "######################"
+echo $yellow "######################"
+echo $yellow "###"$black "=============="$yellow "###"
+echo $yellow "###"$black "=============="$yellow "###"
+echo $yellow "######################"
+# coddingan termux dan kali linux
+echo $black "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo $red "Author : Mr.T1T4N"
+echo $red "Team :"$green "Mugiwara BlackHat" $black "|"$green "Legend Cyber Team"$black "|" $green "Indonesia Learning"
+echo $red "Mastah : JanganLah Kau Menyerah, Dan Mulailah Dari Yang Tergampang "
+echo $black "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo $black "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo $yellow "1]. Tools Banyak Tapi Dikit"
+echo $yellow "2]. DDOS Spesial Banyak"
+echo $yellow "3]. DDOS SPOON"
+echo $yellow "4]. Tools DarkFly"
+echo $yellow "5]. DDOS MugiWARA"
+echo $yellow "6]. Install SOCK"
+echo $yellow "7]. DDOS PentMenu"
+echo $yellow "8]. DDOS Hammer"
+echo $yellow "9]. DDOS LITTEDDDOS"
+echo $yellow "10]. Script Deface Keren"
+echo $yellow "11]. Hack CCTV"
+echo $yellow "12]. Hack CCTV PUB"
+echo $yellow "13]. Hack CCTV Negara"
+echo $yellow "14]. Tools SPAM Plus"
+echo $yellow "15]. Sadap WA Pacar"
+echo $yellow "16]. Virtex Aja Lahh"
+echo $yellow "17]. Virus Link"
+echo $yellow "18]. Spam No Telp"
+echo $yellow "19]. DDOS High ID"
+echo $yellow "20]. DDOS Low ID"
+echo $yellow "21]. Nuyul BTC"
+echo $yellow "22]. Pulsa Gratis"
+echo $yellow "99]. Keluar Aja"
+echo $black "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo $green "Pilih Aja Seterah"
+read -p "Pilih =>" keju
+if [ $keju = 1 ]
+then
+   wget https://raw.githubusercontent.com/WhoAmI-T1T4N/TooLs-D1K1T/main/Tools-Sultan.sh
+    sh Tools-Sultan.sh
+fi
+if [ $keju = 2 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N/ToolS-ID2/main/Tools-4J4X.sh
+   sh Tools-4J4X.sh
+fi
+if [ $keju = 3 ]
+  wget https://raw.githubusercontent.com/BLACK-PHANTOM-CYBER/DDOS-BPC/master/SPOONDDOS.py
+   python2 SPOONDDOS.py
+fi
+if [ $keju = 4 ]
+  wget https://raw.githubusercontent.com/Ranginang67/DarkFly-Tool/master/install.py
+   python2 install.py
+     darkfly
+fi
+if [ $keju = 5 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N/DDoS-EPIC-ID/main/MuGiWARA-DDOS.py
+    python2 MuGiWARA-DDOS.py
+fi
+if [ $keju = 6 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N/SoCKS/main/sockss.py
+    python2 sockss.py
+fi
+if [ $keju = 7 ]
+then
+  wget https://raw.githubusercontent.com/GinjaChris/pentmenu/master/pentmenu
+    ./pentmenu
+fi
+if [ $keju = 8 ]
+then
+  wget https://raw.githubusercontent.com/cyweb/hammer/master/hammer.py
+    python3 hammer.py
+fi
+if [ $keju = 9 ]
+then
+  wget https://raw.githubusercontent.com/4L13199/LITEDDOS/master/LITEDDOS.py
+    python2 LITEDDOS.py
+fi
+if [ $keju = 10 ]
+then
+  wget https://raw.githubusercontent.com/4L13199/LITESCRIPT/master/LITESCRIPT.py
+   python2 LITESCRIPT.py
+fi
+if [ $keju = 11 ]
+then
+  wget https://raw.githubusercontent.com/MrVirusSpm-07/cchek/main/cchek.sh
+    sh cchek.sh
+fi
+if [ $keju = 12 ]
+then
+  wget https://raw.githubusercontent.com/MrVirusSpm-07/cctv-pub/main/cctv.py
+    python2 cctv.py
+fi
+if [ $keju = 13 ]
+then
+  wget https://raw.githubusercontent.com/AngelSecurityTeam/Cam-Hackers/master/cam-hackers.py
+    python3 cam-hackers.py
+fi
+if [ $keju = 14 ]
+then
+  wget https://raw.githubusercontent.com/4L13199/LITESPAM/master/LITESPAM.sh
+    bash LITESPAMM.sh
+fi
+if [ $keju = 15 ]
+then
+  wget https://raw.githubusercontent.com/Anonymous3-SIT/MrSadapWhatsApp/master/MrSadapWhatsApp.sh
+    sh MrSadapWhatsApp.sh
+fi
+if [ $keju = 16 ]
+then
+  termux-setup-storage
+    git clone https://github.com/MR-X-junior/Virtex
+     cd Virtex
+      python3 Virtex.py
+fi
+if [ $keju = 17 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N/Virus-DangeRS/main/Virus-HOKI.sh
+    sh Virus-HOKI.sh
+fi
+if [ $keju = 18 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N/SpamV2/main/NelponNgasal.sh
+    bash NelponNgasal.sh
+fi
+if [ $keju = 19 ]
+then
+  wget https://raw.githubusercontent.com/BLACK-PHANTOM-CYBER/DDOS2/master/high.py
+    python2 high.py
+fi
+if [ $keju = 20 ]
+then
+  wget https://raw.githubusercontent.com/BLACK-PHANTOM-CYBER/DDOS2/master/low.py
+    python2 low.py
+fi
+if [ $keju = 21 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N/BitSlayer/main/BTC_Slayer.py
+    python BTC_Slayer.py
+fi
+if [ $keju = 22 ]
+then
+  wget https://raw.githubusercontent.com/MrTamfanX/PulsanyaMrTamfan/master/PulsaOmTamfan.sh
+    sh PulsaOmTamfan
+fi
+if [ $keju = 99 ]
+then
+echo $green "TTTTTTTTTTTTTTTTTTTTTTT"$yellow "EEEEEEEEEEEEEEEEEEEEE"$green "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"$red "WWWWWW"        "WWWWWWWWW"          "WWWWWWWWW"
+echo $green "TTTTTTTTTTTTTTTTTTTTTTTT"$yellow "EEEEEEEEEE"          $green "TTTT   TTTTTTTTTTTTTT     TTTT"$red "WWWWWW"        "WWWWWWWWW"          "WWWWWWWWW"
+echo $green "TTTTTTTTTTTTTTTTTTTTTTT"$yellow "EEEEEEEEEE"           $green "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"$red "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo $green "     TTTTTTTTT         "$yellow "EEEEEEEEEEEEEEEEEEEE"$green "          TTTTTTTTTTT          "$red "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo $green "     TTTTTTTTT         "$yellow "EEEEEEEEEEEEEEEEEEEE"$green "           TTTTTTTTTT          "$white    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo $green "     TTTTTTTTT         "$yellow "EEEEEEEEE"           $green "           TTTTTTTTTT          "$white    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo $green "     TTTTTTTTT         "$yellow "EEEEEEEEEEEEEEEEEEE"                                         $white    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo $green "     TTTTTTTTT         "$yellow "EEEEEEEEEEEEEEEEEEE"
